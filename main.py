4553fad5dcadfed65bf8ae279dd7735b1a2ba374
Mohammed <khamismitch@gmail.com>
2024-12-15 05:37:21 +0300

    complete

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+
+# function to downlaod the file
+def download_file(url, filename= ''):
+    response = requests.get(url, stream= True)
+    # pass a new filename or maitain the default
+    if filename :
+        pass
+    else:
+        filename = url.split('/')[-1]
+
+    # store the file
+    with open(filename, 'wb') as file:
+        # download the file in small chunks
+        for chunk in response.iter_content(chunk_size= 8192):
+            file.write(chunk)
+    
+if __name__ == '__main__':
+    download_url = input("Enter the url: ").strip()
+    save_name = input("Enter a save name (leave blank to use default): ").strip()
+    save_to = save_name if save_name else None
+
+
+    download_file(download_url, save_to)
